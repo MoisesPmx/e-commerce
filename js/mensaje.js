dc36d46fc83a466ed1c0c42807d6ba3cbca8e04a
Moyo <berryz@vivaldi.net>
2023-03-14 10:36:02 -0600

    Formularios validados

--- js/mensaje.js
+++ js/mensaje.js
@@ -1,12 +1,15 @@
 const boton = document.getElementById("formulario__boton");
-boton.addEventListener("click", function validar() {
+boton.addEventListener("click", function validar(evento) {
+
+    evento.preventDefault();
+
     var nombre = document.getElementById("formulario__nombre");
     var mensaje = document.getElementById("formulario__textarea");
     if (nombre.value == "" || mensaje.value == "") {
         alert("Debes llenar todos los campos");
         
     } else {
-        alert("Gracias por tu mensaje");
-        //window.location.href = "../enviado.html";
+        
+        window.location.href = "../enviado.html";
     }
 });
--- js/usuario.js
+++ js/usuario.js
@@ -1,15 +1,17 @@
 const botonx = document.getElementById("login__submit");
-botonx.addEventListener("click", () => {
+botonx.addEventListener("click", (evento) => {
+    evento.preventDefault();
     var usuario = document.getElementById("usuario").value;
     var contrasenia = document.getElementById("contrasenia").value;
 
-    var usuarioCorrecto = "admin@mail.com";
+    var usuarioCorrecto = "a@mail.com";
     var contraseniaCorrecta = "1234";
 
     if (usuario == usuarioCorrecto && contrasenia == contraseniaCorrecta) {
-        
-        window.location.href = "/agregar.html";
+        window.location.href = "../agregar.html";
+        return true;
     } else {
         alert("Usuario y/o contraseña incorrectos");
     }
+    return false;
 });
