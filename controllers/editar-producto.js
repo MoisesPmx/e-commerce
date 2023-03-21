db29754b904f15305cfa240eaa7ab6a61acee30b
Moyo <berryz@vivaldi.net>
2023-03-20 18:51:43 -0600

    archivos js terminados

---
+++ controllers/editar-producto.js
@@ -0,0 +1,47 @@
+import { productoServicio } from "../service/productos.js";
+
+const formulario = document.querySelector("[data-form]");
+
+const infoProducto = () => {
+    const url = new URL(window.location);
+    const idnt = url.searchParams.get("identificador");
+
+    if (idnt === null) {
+        window.location.href = "/error.html";
+    }
+
+    const nombre = document.querySelector("[data-nombre]");
+    const categoria = document.querySelector("[data-categoria]");
+    const imagen = document.querySelector("[data-imagen]");
+    const precio = document.querySelector("[data-precio]");
+    const descripcion = document.querySelector("[data-descripcion]");
+
+    productoServicio.datosProducto(idnt).then((producto) => {
+        nombre.value = producto.nombre;
+        categoria.value = producto.categoria;
+        imagen.value = producto.imagen;
+        precio.value = producto.precio;
+        descripcion.value = producto.descripcion;
+    });
+};
+
+infoProducto();
+
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    const url = new URL(window.location);
+    const idnt = url.searchParams.get("identificador");
+
+    const nombre = document.querySelector("[data-nombre]").value;
+    const categoria = document.querySelector("[data-categoria]").value;
+    const imagen = document.querySelector("[data-imagen]").value;
+    const precio = document.querySelector("[data-precio]").value;
+    const descripcion = document.querySelector("[data-descripcion]").value;
+
+    console.log(nombre, categoria, imagen, precio, descripcion);
+    productoServicio
+        .updateProducto(categoria, imagen, nombre, precio, descripcion, idnt)
+        .then(() => {
+            window.location.href = "cambio-exitoso.html";
+        });
+});
